9e33089801189a54ade31dc1643af9a1c854cbc8
NikoleK <sergo901@yandex.ru>
2018-01-28 18:06:15 +0100

    update window size

--- src/main/java/driver/BrowserHelper.java
+++ src/main/java/driver/BrowserHelper.java
@@ -1,6 +1,7 @@
 package driver;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -17,7 +18,7 @@ public class BrowserHelper {
 
     public static void openWindow(String url){
         driver = new FirefoxDriver();
-        driver.manage().window().fullscreen();
+        driver.manage().window().setSize(new Dimension(400,300));
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         driver.manage().deleteAllCookies();
         driver.get(url);
